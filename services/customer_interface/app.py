bb818090e58bf05a83b69e33298eb712e5f5168b
fSidel <118626506+fSidel@users.noreply.github.com>
2025-04-14 00:10:28 +0200

    barebones navbar woohoo

--- services/customer_interface/app.py
+++ services/customer_interface/app.py
@@ -4,6 +4,14 @@ import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def time():
+def home():
     current_time = datetime.datetime.now()
     return render_template('welcome.html', current_time=current_time)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
