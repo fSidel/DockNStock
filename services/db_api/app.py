00c30a119ebe676efcf661430f72d8f8ab1fabb5
fSidel <118626506+fSidel@users.noreply.github.com>
2025-04-25 12:16:11 +0200

    change images and try to initialize db_api

--- services/db_api/app.py
+++ services/db_api/app.py
@@ -4,8 +4,9 @@ from os import environ
 from flask_login import LoginManager, UserMixin, login_user, logout_user   
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+
 
 class Users(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -19,5 +20,27 @@ class Users(UserMixin, db.Model):
 
     def __repr__(self):
         return f'<User {self.username}, password {self.password}>'
+    
 
-db.create_all()
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)    
+    name = db.Column(db.String(20), nullable = False)
+    weight = db.Column(db.String(20), nullable = False)
+    photo = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String(250), nullable = False)
+
+    def __init__(self, name=None, weight=None, photo=None, description=None):
+        self.name = name
+        self.weight = weight
+        self.photo = photo
+        self.description = description
+
+    def __repr__(self):
+        return f'<Product {self.name}, weight: {self.weight}, photo: {self.photo}, description: {self.description}>'
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create all tables
+    app.run(host='0.0.0.0', port=5000, debug=True)
