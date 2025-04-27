3a9dbe3901873c9ab913f3b7f164374201152947
giovannicanedoli <gio.canedoli02@gmail.com>
2025-04-27 20:48:09 +0200

    adding login

--- services/customer_interface/app.py
+++ services/customer_interface/app.py
@@ -6,8 +6,17 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user
 from datetime import *
 import random
 import os
+import requests
+import re
 
 app = Flask(__name__)
+
+def verify_password(password):
+    if re.match(r'^(?=.*[A-Z])(?=.*\W).{8,}$', password):
+        return True
+    else:
+        return False
+    
 
 #register page
 @app.route('/register', methods=["GET", "POST"])
@@ -17,33 +26,36 @@ def register():
         username = request.form.get('username_input')
         password = request.form.get("password_input1")
         password_verify = request.form.get("password_input2")
-
-        #cambiare
         password_ok = verify_password(password_verify)
 
-        #vedere come fare la query
-        user = Users.query.filter_by(username = username).first()
+        json_data = {
+            'username': 'giovannagher',
+            'password': 'canedoli',
+        }
 
-        if user:
+        user = requests.post('http://db_api:5000/users/login', json=json_data)
+
+        if user.ok:
             return render_template("signup.html", user_alive = True)
 
         if password == password_verify and password_ok:
-            user = Users(username, password)
-            db.session.add(user)
-            db.session.commit()
+            user = requests.post('http://db_api:5000/users/register', json=json_data)
+            if user.ok:
+                #vedere come gestire
+                session.permanent = True
+                
+                session['username'] = username
+                session['password'] = password
+                session['id'] = user['user']['id'] #?
 
-            #vedere come gestire
-            session.permanent = True
-            
-            session['username'] = username
-            session['password'] = password
-            session['id'] = user.id
-
-            return redirect(url_for("main_route"))
+                #return redirect(url_for("main_route"))
+                return "ok till here"
         else:
             return render_template("signup.html", password_ok=password_ok, password=password, password_verify=password_verify)
     elif 'username' in session and 'password' in session:
-        return redirect(url_for("main_route"))
+        return "bo"
+        #return redirect(url_for("main_route"))
     else:
         return render_template("signup.html")
     
+app.run(debug=True, host="0.0.0.0")
