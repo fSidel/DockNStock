e95d7bb90fd0e473966b3c2d138857f8fd37f43e
fSidel <118626506+fSidel@users.noreply.github.com>
2025-05-06 04:15:03 +0200

    no need for login logic in database

--- services/db_api/app.py
+++ services/db_api/app.py
@@ -1,7 +1,5 @@
 from os import environ
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask import Flask
 from database import db
 from models import Users, Products
 from routes.user import user_bp
@@ -15,12 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@db:5432/
 db.init_app(app)  
 with app.app_context():
     db.create_all()
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-@login_manager.user_loader
-def load_user(user_id):
-    return Users.query.get(int(user_id))
 
 app.register_blueprint(user_bp)
 app.register_blueprint(product_bp)
