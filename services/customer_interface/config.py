3c1745ab781ec427910e8620b507f9d0dcc9b89c
fSidel <118626506+fSidel@users.noreply.github.com>
2025-05-06 09:04:29 +0200

    ssl config

--- services/customer_interface/config.py
+++ services/customer_interface/config.py
@@ -5,5 +5,6 @@ class Config:
     MAIL_SERVER = os.getenv('MAIL_SERVER')
     MAIL_PORT = os.getenv('MAIL_PORT')
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
