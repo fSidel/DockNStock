66b5cca5c00b9b9357c332f690088db04b1d6abc
fSidel <118626506+fSidel@users.noreply.github.com>
2025-05-06 09:54:12 +0200

    change config

--- services/customer_interface/config.py
+++ services/customer_interface/config.py
@@ -3,8 +3,7 @@ import os
 class Config:
     SECRET_KEY = "STOCKNDOCK :)"
     MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = os.getenv('MAIL_PORT')
-    MAIL_USE_TLS = True
+    MAIL_PORT = 465
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
