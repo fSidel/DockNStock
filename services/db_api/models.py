7fb35451d06ff9c60f5ac4d4c5327cebd16e5cf5
fSidel <118626506+fSidel@users.noreply.github.com>
2025-05-06 04:20:33 +0200

    remove unused imports

--- services/db_api/models.py
+++ services/db_api/models.py
@@ -14,6 +14,9 @@ class Users(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+    
+    def username_exists(self, username):
+        return db.session.query(Users).filter_by(username=username).first() is not None
 
     def __init__(self, username=None, password=None):
         self.username = username
