0e31949351f504977e44f97bd4ac49afcc1de76b
fSidel <118626506+fSidel@users.noreply.github.com>
2025-05-06 05:31:28 +0200

    add set_password route in api

--- services/db_api/routes/user.py
+++ services/db_api/routes/user.py
@@ -45,3 +45,17 @@ def present():
         return jsonify({'message': 'Logged in', 'user': user.to_dict()}), 200
     return jsonify({'error': 'User not found'}), 401
     
+
+@user_bp.route("/users/change_password", methods=["POST"])
+def change_password():
+    data = request.get_json()
+    username = data.get("username")
+    new_password = data.get("new_password")
+
+    user = Users.query.filter_by(username=username).first()
+    if user:
+        user.set_password(new_password)
+        db.session.commit()
+        return jsonify({'message': 'Password changed successfully'}), 200
+    
+    return jsonify({'error': 'User not found'}), 404
