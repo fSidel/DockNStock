77f50b240ebdcec431377136ad4a6f04003b65cb
fSidel <118626506+fSidel@users.noreply.github.com>
2025-05-06 05:01:32 +0200

    add token generation and verification logic

---
+++ services/customer_interface/models.py
@@ -0,0 +1,30 @@
+from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from config import Config
+
+class User(UserMixin):
+    def __init__(self, id, username):
+        self.id = id
+        self.username = username
+
+    @staticmethod
+    def from_dict(data):
+        """Create a User object from a dictionary."""
+        return User(id=data.get("id"), username=data.get("username"))
+
+    def to_dict(self):
+        """Convert the User object to a dictionary."""
+        return {"id": self.id, "username": self.username}
+    
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(Config.SECRET_KEY, expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(Config.SECRET_KEY)
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
